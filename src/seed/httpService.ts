c20d27591bf010e44d703e37c181a5e835e05f12
joydipand@gmail.com <joydipand@gmail.com>
2017-08-04 13:09:59 +0530

    chore(): made rest service and comented code properly

--- src/seed/httpService.ts
+++ src/seed/httpService.ts
@@ -8,44 +8,148 @@ import 'rxjs/add/observable/throw';
 // app config
 import { BASE_URL } from '../app/config';
 
+/**
+ * Service Seed
+ * @class
+ */
 @Injectable()
-export class PeopleService {
+export class ServiceName {
     private baseUrl: string = BASE_URL;
-
+    /** @constructs */
     constructor(private http: Http) { }
 
+    /**
+     * Header object
+     * @description get http header object
+     * @private
+     * @param none
+     * @return {object} header - Default: application/json
+     */
     private getHeaders() {
         let headers = new Headers();
         headers.append('Accept', 'application/json');
         return headers;
     }
 
-    // this could also be a private method of the component class
+    /**
+     * Extract data
+     * @description extract data from response
+     * @private
+     * @param {Response} res
+     * @return {object} body - Default: {}
+     */
+    private extractData(res: Response) {
+        return res.json() || {};
+    }
+
+    /**
+     * Handle error
+     * @description this could also be a private method of the component class
+     * @private
+     * @param {Response} error
+     * @return {object} body - Default: {}
+     */
     private handleError(error: Response) {
         // throw an application level error
         return Observable.throw(error.json().error || 'Opps got an error on api');;
     }
 
+    /**
+     * Get all data
+     * @description getAll service method
+     * @param none
+     * @return {any} Observable 
+     * @example 
+     * this.serviceName
+     *      .getAll()
+     *      .subscribe(
+     *          p => console.log(p),
+     *          e => console.log(e)
+     *       );
+     */
     getAll(): Observable<any> {
         return this.http
             .get(`${this.baseUrl}/<service_name>`, { headers: this.getHeaders() })
-            .map(res => res.json())
+            .map(this.extractData)
             .catch(this.handleError);
     }
 
+    /**
+     * Get data
+     * @description get service method
+     * @param {number} id - get item id
+     * @return {any} Observable 
+     * @example 
+     * this.serviceName
+     *      .get(<id>)
+     *      .subscribe(
+     *          p => console.log(p),
+     *          e => console.log(e)
+     *       );
+     */
     get(id: number): Observable<any> {
         return this.http
             .get(`${this.baseUrl}/<service_name>/${id}`, { headers: this.getHeaders() })
-            .map(res => res.json())
+            .map(this.extractData)
             .catch(this.handleError);
     }
 
-    save(person: any): Observable<any> {
-        // this won't actually work because the StarWars API 
-        // is read-only. But it would look like this:
+    /**
+     * Save data
+     * @description save service method
+     * @param {any} data - This is data object 
+     * @return {any} Observable 
+     * @example 
+     * this.serviceName
+     *      .get(data)
+     *      .subscribe(
+     *          p => console.log(p),
+     *          e => console.log(e)
+     *       );
+     */
+    save(data: any): Observable<any> {
         return this.http
-            .put(`${this.baseUrl}/<service_name>/${person.id}`, JSON.stringify(person), { headers: this.getHeaders() })
+            .put(`${this.baseUrl}/<service_name>/${data.id}`, JSON.stringify(data), { headers: this.getHeaders() })
             .catch(this.handleError);
     }
 
+    /**
+     * Delete data
+     * @description delete service method
+     * @param {number} id - Delete item id
+     * @return {any} Observable 
+     * @example 
+     * this.serviceName
+     *      .delete(id)
+     *      .subscribe(
+     *          p => console.log(p),
+     *          e => console.log(e)
+     *       );
+     */
+    delete(id: number): Observable<any> {
+        return this.http
+            .delete(`${this.baseUrl}/<service_name>/${id}`, { headers: this.getHeaders() })
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    /**
+     * Patch data
+     * @description patch service method
+     * @param {number} id - Delete item id
+     * @return {any} Observable 
+     * @example 
+     * this.serviceName
+     *      .patch({id: '12' , name: 'abc'})
+     *      .subscribe(
+     *          p => console.log(p),
+     *          e => console.log(e)
+     *       );
+     */
+    patch(data: any): Observable<any> {
+        return this.http
+            .patch(`${this.baseUrl}/<service_name>/${data.id}`, data, { headers: this.getHeaders() })
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
 }
